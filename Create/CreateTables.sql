55004ccef2b8f14425f0dc13df0d034ca469a60b
Felipe Oliveira de Souza <fo.souza@outlook.com>
2020-04-20 23:35:12 -0300

    Criacao da base de teste

---
+++ Create/CreateTables.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE FaculdadeDB
+GO
+
+USE FaculdadeDB
+GO
+
+CREATE TABLE dbo.Alunos (
+    Id          bigint        not null identity(1,1) primary key,
+    Nome        nvarchar(150) not null,
+    DataNasc    date          not null,
+    Celular     nvarchar(20)  not null,
+    Email       nvarchar(50)  not null,
+    DataCriacao date          not null default(getdate())
+)
+GO
+
+CREATE TABLE dbo.Professores (
+    Id          bigint        not null identity(1,1) primary key,
+    Nome        nvarchar(150) not null,
+    Celular     nvarchar(20)  not null,
+    Email       nvarchar(50)  not null,
+    DataCriacao date          not null default(getdate())
+)
+GO
+
+CREATE TABLE dbo.Cursos (
+    Id          bigint        not null identity(1,1) primary key,
+    Nome        nvarchar(150) not null,
+    DuracaoAnos numeric(1,1)   not null,
+    ProfessorId bigint        not null,
+    DataCriacao date          not null default(getdate())    
+)
+GO
+
+ALTER TABLE dbo.Cursos
+ADD CONSTRAINT FK_PROFESSOR FOREIGN KEY (ProfessorId) REFERENCES dbo.Professores (Id)
+GO
+
+CREATE TABLE dbo.CursosAlunos (
+    CursoId         bigint not null,
+    AlunoId     bigint not null   
+)
+GO
+
+ALTER TABLE dbo.CursosAlunos
+ADD CONSTRAINT PK_Cursos_Alunos PRIMARY KEY (CursoId, AlunoId)
+GO
+
+ALTER TABLE dbo.CursosAlunos
+ADD CONSTRAINT FK_Cursos_Alunos FOREIGN KEY (CursoId) REFERENCES dbo.Cursos (Id)
+GO
+
+ALTER TABLE dbo.CursosAlunos
+ADD CONSTRAINT FK_Alunos_Cursos FOREIGN KEY (AlunoId) REFERENCES dbo.Alunos (Id)
+GO
---
+++ Insert/InsertRegisters.sql
@@ -0,0 +1,39 @@
+USE FaculdadeDB
+GO
+
+INSERT INTO dbo.Alunos (Nome, DataNasc, Celular, Email) VALUES
+('Maria Aparecida de Santos', '1989-07-13', '(11) 97845-4512', 'maria@dev.com.br'),
+('João Mendes de Fibini', '1989-07-13', '(11) 97845-4512', 'joão@dev.com.br'),
+('José Pinto', '1989-07-13', '(11) 97845-4512', 'josé@dev.com.br'),
+('Juliana Alencar', '1989-07-13', '(11) 97845-4512', 'juliana@dev.com.br'),
+('Patricia Brito', '1989-07-13', '(11) 97845-4512', 'patricia@dev.com.br'),
+('Luiza Kaori Sato', '1989-07-13', '(11) 97845-4512', 'luiza@dev.com.br'),
+('Sabrina Santos de Souza', '1989-07-13', '(11) 97845-4512', 'sabrina@dev.com.br'),
+('Felipe Oliveira de Souza', '1989-07-13', '(11) 97845-4512', 'felipe@dev.com.br')
+GO
+
+INSERT INTO dbo.Professores (Nome, Celular, Email) VALUES
+('Mariana Jujuba', '(11) 97845-4512', 'mariana@dev.com.br'),
+('Marcos Cesar Augustos', '(11) 97845-4512', 'marcos@dev.com.br'),
+('Marcelo Xavier', '(11) 97845-4512', 'marcelo@dev.com.br'),
+('Ana Maria Bunda', '(11) 97845-4512', 'ana@dev.com.br'),
+('Roneiva Runiania', '(11) 97845-4512', 'roneiva@dev.com.br'),
+('Cecilia Batista', '(11) 97845-4512', 'cecilia@dev.com.br'),
+('Paulo Fabiosi', '(11) 97845-4512', 'paulo@dev.com.br'),
+('Luciana Murilais', '(11) 97845-4512', 'luciana@dev.com.br')
+GO
+
+INSERT INTO dbo.Cursos (Nome, DuracaoAnos, ProfessorId) VALUES
+('Gastronomia', 6.5, 1),
+('Contabilidade', 4.5, 1),
+('Fisioterapia', 2, 2),
+('Radiologia', 1.5, 2),
+('Análise de Sistemas', 4, 3),
+('Banco de dados', 4.5, 4),
+('Arquiteto de software', 8, 4),
+('Engenharia de software', 6, 4),
+('Psicologia', 3, 5),
+('Pedagogia', 2, 6),
+('Administração', 2, 6),
+('Engenharia', 7, 7)
+GO
