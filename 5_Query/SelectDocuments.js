96eec46d5d48ac59893be699622f2e2eac6e5b47
Felipe Oliveira de Souza <fo.souza@outlook.com>
2020-05-13 19:41:14 -0300

    Estudo order by

--- 5_Query/SelectDocuments.js
+++ 5_Query/SelectDocuments.js
@@ -1,9 +1,12 @@
 use FaculdadeDB
 
+// 1
 db.Cursos.find()
 
+// 2
 db.Cursos.find({ Valor: 2000})
 
+// 3
 db.Cursos.find({ Valor: { $lt: 5000 } })
 
 db.Cursos.find({ Valor: { $gt: 4000 } })
@@ -12,18 +15,27 @@ db.Cursos.find({ Valor: { $gte: 5000 } })
 
 db.Cursos.find({ Valor: { $lte: 5000 } })
 
+// 4
 db.Cursos.find({ Valor: { $gte: 3000, $lte: 4000 } })
 db.Cursos.find({ $and: [ 
     { Valor: { $gte: 3000 } }, 
     { Valor: { $lte: 4000 } } 
 ] })
 
+// 5
 // Consulta com like no SQL é usada como expressão regular no mongodb, estudar regex
 
+// 6
 db.Cursos.find({ DuracaoAnos: { $in: [ 2,3,4 ] } })
 
 db.Cursos.find({ DuracaoAnos: { $not: { $in: [ 2,3,4 ] } } })
 
+// 7
 db.Cursos.find({ ProfessorId: null })
 
 db.Cursos.find({ ProfessorId: { $ne: null } })
+
+// 8
+db.Cursos.find().sort({ DuracaoAnos: 1 })
+
+db.Cursos.find().sort({ DuracaoAnos: -1 })
--- 5_Query/SelectRegisters.sql
+++ 5_Query/SelectRegisters.sql
@@ -1,10 +1,13 @@
 USE FaculdadeDB
 GO
 
+-- 1
 SELECT * FROM dbo.Cursos
 
+-- 2
 SELECT * FROM dbo.Cursos WHERE Valor = 2000
 
+-- 3
 SELECT * FROM dbo.Cursos WHERE Valor < 5000
 
 SELECT * FROM dbo.Cursos WHERE Valor > 4000
@@ -13,14 +16,23 @@ SELECT * FROM dbo.Cursos WHERE Valor >= 5000
 
 SELECT * FROM dbo.Cursos WHERE Valor <= 5000
 
+-- 4
 SELECT * FROM dbo.Cursos WHERE Valor BETWEEN 3000 AND 4000
 
+-- 5
 SELECT * FROM dbo.Cursos WHERE Nome LIKE 'A%'
 
+-- 6
 SELECT * FROM dbo.Cursos WHERE DuracaoAnos IN (2, 3, 4)
 
 SELECT * FROM dbo.Cursos WHERE DuracaoAnos NOT IN (2, 3, 4)
 
+-- 7
 SELECT * FROM dbo.Cursos WHERE ProfessorId IS NULL
 
 SELECT * FROM dbo.Cursos WHERE ProfessorId IS NOT NULL
+
+-- 8
+SELECT * FROM dbo.Cursos ORDER BY DuracaoAnos
+
+SELECT * FROM dbo.Cursos ORDER BY DuracaoAnos DESC
